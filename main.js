c0ddd37abebc36d6c9f283847fcfdc7d76a140b4
tyulari <128141264+tyulari@users.noreply.github.com>
2023-04-21 14:34:08 +0300

    add accordion in hero section

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ MENU_LINKS.forEach((link) => link.addEventListener('click', event => {
         behavior:"smooth"
     })
 }));
+
+/*аккордеон*/
+const HERO_DOTS = document.querySelectorAll('.hero__dot');
+const HERO_IMG = document.querySelector('.hero__image');
+const HERO_TITLE = document.querySelector('.hero__title');
+const HERO_SUBTITLE = document.querySelector('.hero__subtitle');
+
+const TITLE = [
+    'Разработано 2000+ приложений',
+    'Бесплатные приложения',
+    'Тысячи довольных клиентов'
+];
+
+const SUBTITLE = [
+    "Для вашего удобства мы разработали множество приложений для вашего бизнеса и команды, которые доступны в любое время суток",
+    'К любому сервисному обслуживанию мы подключим советы по управлению компанией совершенно бесплатно',
+    'Накопленный нами опыт позволяет предлагать клиентам обширный спектр качественных приложений и сервисов'
+]
+
+HERO_DOTS.forEach((dot, index) => dot.addEventListener('click', () => {
+   //убрать класс active
+    HERO_DOTS.forEach(el => el.classList.remove('active'));
+    // добавить к нажатой
+    dot.classList.add('active');
+    
+    HERO_IMG.src = `assets/hero_${index+1}.jpg`;
+    HERO_TITLE.textContent = TITLE[index];
+    HERO_SUBTITLE.textContent = SUBTITLE[index];
+}));
+
+
